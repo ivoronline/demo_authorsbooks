a6754d44657e75fd1390c07116dff98dd810c545
igoreta <ivoronline@gmail.com>
2021-02-17 19:21:13 +0100

    Commit

---
+++ src/main/java/com/ivoronline/demo_authorsbooks/controllers/TestController.java
@@ -0,0 +1,25 @@
+package com.ivoronline.demo_authorsbooks.controllers;
+
+import com.ivoronline.demo_authorsbooks.entities.Author;
+import com.ivoronline.demo_authorsbooks.services.AuthorServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.Date;
+
+@Controller
+public class TestController {
+
+  //======================================================================
+  // METHOD: TEST
+  //======================================================================
+  @ResponseBody
+  @RequestMapping("/Test")
+  public String test() {
+    return "Hello from Application: Authors Books <br> Branch: Step1_Basic";
+  }
+
+}
